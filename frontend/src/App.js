4ff6d708e0fd0ff97828a1229f165d7875e2f5b3
Anshul-Singla <asingla2807@gmail.com>
2023-01-16 21:46:49 +0530

    boiler plate

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import { Box, Heading } from '@chakra-ui/react';
+
+function App() {
+  return (
+    <Box className="App">
+      <Heading>life_is_awesome...:-)</Heading>
+    </Box>
+  );
+}
+
+export default App;
